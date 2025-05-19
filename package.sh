ac9372db0eb5c91b10b05dedba4aac429259a350
Otto kai <121492489+yhlh9982@users.noreply.github.com>
2025-05-19 19:45:16 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# iStore
+#echo "src-git istore https://github.com/linkease/istore;main" >> feeds.conf.default
+
 git clone --depth 1 https://github.com/bigbugcc/OpenwrtApp package/otherapp/OpenwrtApp
 git clone --depth 1 https://github.com/destan19/OpenAppFilter package/otherapp/OpenAppFilter
 git clone --depth 1 https://github.com/zzsj0928/luci-app-pushbot package/otherapp/luci-app-pushbot
