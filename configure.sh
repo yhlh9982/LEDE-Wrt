a27290a067444bdeb26683562e7992edff5a27b8
Otto kai <121492489+yhlh9982@users.noreply.github.com>
2025-05-19 10:03:11 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -22,5 +22,5 @@ echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >
 #echo "src-git istore https://github.com/linkease/istore;main" >> feeds.conf.default
 
 # 替换默认主题
-rm -rf package/lean/luci-theme-argon 
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon
+#rm -rf package/lean/luci-theme-argon 
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon
