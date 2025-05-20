c4282622ef0e42d2202d68d17aeaed4598e549ec
Otto kai <121492489+yhlh9982@users.noreply.github.com>
2025-05-20 19:13:34 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-# luci-theme-neobird
-git clone --depth 1 https://github.com/thinktip/luci-theme-neobird.git package/otherapp/luci-theme-neobird
 # luci-theme-argon
 rm -rf feeds/luci/themes/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
